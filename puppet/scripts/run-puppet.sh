7b667dd0c7371de28b32a4335295232121c847ee
Asrael <asrael@hyper.equipment>
2023-01-12 17:51:44 -0800

    fix my dumb script

--- puppet/scripts/run-puppet.sh
+++ puppet/scripts/run-puppet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-REPO_ROOT="$(dirname -- "$(dirname -- "$( readlink -f -- "${0}"; )")")"
+REPO_ROOT="$(git -C "$( readlink -f -- "${0}"; )" rev-parse --show-toplevel)"
 PUPPET_DISABLEFILE="/puppet-disabled"
 cd "$REPO_ROOT"
 
